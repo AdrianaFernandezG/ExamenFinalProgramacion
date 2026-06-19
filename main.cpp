66ec4e61cf8d8ccb4781ec0bff3c572fb915c241
santiagorojasb-cmyk <santiago.rojas.b@ucb.edu.bo>
2026-06-18 23:04:41 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -11,8 +11,11 @@
 #include "vectores.h"
 using namespace std;
 
+//Propósito: Punto de entrada principal que inicializa y arranca la ejecución del sistema.
+//Parámetro: Ninguno.
+//Retorno: Entero 0 como señal de finalización exitosa del programa.
 int main(){
-	mostrarIntroduccion();
-    opcionesMenu();
-    return 0;
+    mostrarIntroduccion(); // Despliega carátula
+    opcionesMenu();        // Entra al bucle lógico del menú principal
+    return 0;              // Fin del ciclo de vida de la app
 }
